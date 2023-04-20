5963f65661ca5eb462d231323eb51577314be815
EgoFox <egotdp4@gmail.com>
2023-04-20 17:43:38 +0200

    Init some components

--- bank_landing_page/src/App.jsx
+++ bank_landing_page/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+//import React from "react";
+import styles from "./style";
+
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>NavBar</div>
+      </div>
 
-export default App
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats Business Billing CardDeal Testimonials Clients CTA Footer
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
