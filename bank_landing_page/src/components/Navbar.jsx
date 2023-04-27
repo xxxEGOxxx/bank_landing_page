1949a92f96754595a7c8b698e242355a2de51618
EgoFox <egotdp4@gmail.com>
2023-04-27 11:22:44 +0200

    Add the stats section

--- bank_landing_page/src/components/Navbar.jsx
+++ bank_landing_page/src/components/Navbar.jsx
@@ -30,6 +30,25 @@ const Navbar = () => {
         object-contain"
           onClick={() => setToggle((prev) => !prev)}
         />
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                }`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
